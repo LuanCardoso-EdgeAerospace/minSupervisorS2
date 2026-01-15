5b6cefd498d8b7e314e1de63651b367c95d40f8f
Luan Cardoso <luan.cardoso@edge-aerospace.com>
2026-01-15 16:59:27 +0100

    TMP468 Driver

--- Core/Inc/TMP468.h
+++ Core/Inc/TMP468.h
@@ -5,9 +5,203 @@
  *      Author: luan
  */
 
-#ifndef INC_TMP468_H_
-#define INC_TMP468_H_
+#ifndef INC_TMP468_HO_
+#define INC_TMP468_HO_
+
+#include<stdint.h>
+
+/******************************** Register map ********************************/
+
+#define TMP468_REG_LOCAL_TMP   0x00
+#define TMP468_REG_REMOTE1_TMP 0x01
+#define TMP468_REG_REMOTE2_TMP 0x02
+#define TMP468_REG_REMOTE3_TMP 0x03
+#define TMP468_REG_REMOTE4_TMP 0x04
+#define TMP468_REG_REMOTE5_TMP 0x05
+#define TMP468_REG_REMOTE6_TMP 0x06
+#define TMP468_REG_REMOTE7_TMP 0x07
+#define TMP468_REG_REMOTE8_TMP 0x08
+
+#define TMP468_REG_SOFTWARE_RESET 0x20
+
+#define TMP468_REG_THERM1_STATUS 0x21
+#define TMP468_REG_THERM2_STATUS 0x22
+
+#define TMP468_REG_REMOTE_CHANNEL_OPEN_STATUS 0x23
+#define TMP468_REG_CONFIGURATION 0x30
+
+#define TMP468_REG_THERM_HYSTERESIS 0x38
+
+#define TMP468_REG_LOCAL_THERM2_LIMIT 0x39
+#define TMP468_REG_LOCAL_THERM1_LIMIT 0x3A
+
+/**************************** Remote Configuratoin ****************************/
+
+#define TMP468_REG_REMOTE1_OFFSET 0x40              
+#define TMP468_REG_REMOTE1_IDEALITY_CORRECTION 0x41/*on datasheet represented by greek leter eta (η)*/
+#define TMP468_REG_REMOTE1_THERM1_LIMIT 0x42
+#define TMP468_REG_REMOTE1_THERM2_LIMIT 0x43
 
 
+#define TMP468_REG_REMOTE2_OFFSET 0x48
+#define TMP468_REG_REMOTE2_IDEALITY_CORRECTION 0x49/*on datasheet represented by greek leter eta (η)*/
+#define TMP468_REG_REMOTE2_THERM1_LIMIT 0x4a
+#define TMP468_REG_REMOTE2_THERM2_LIMIT 0x4b
 
-#endif /* INC_TMP468_H_ */
+
+#define TMP468_REG_REMOTE3_OFFSET 0x50
+#define TMP468_REG_REMOTE3_IDEALITY_CORRECTION 0x51/*on datasheet represented by greek leter eta (η)*/
+#define TMP468_REG_REMOTE3_THERM1_LIMIT 0x52
+#define TMP468_REG_REMOTE3_THERM2_LIMIT 0x52
+
+
+#define TMP468_REG_REMOTE4_OFFSET 0x58
+#define TMP468_REG_REMOTE4_IDEALITY_CORRECTION 0x59/*on datasheet represented by greek leter eta (η)*/
+#define TMP468_REG_REMOTE4_THERM1_LIMIT 0x5a
+#define TMP468_REG_REMOTE4_THERM2_LIMIT 0x5b
+
+
+#define TMP468_REG_REMOTE5_OFFSET 0x60
+#define TMP468_REG_REMOTE5_IDEALITY_CORRECTION 0x61 /*on datasheet represented by greek leter eta (η)*/
+#define TMP468_REG_REMOTE5_THERM1_LIMIT 0x62
+#define TMP468_REG_REMOTE5_THERM2_LIMIT 0x63
+
+
+#define TMP468_REG_REMOTE6_OFFSET 0x68
+#define TMP468_REG_REMOTE6_IDEALITY_CORRECTION 0x69 /*on datasheet represented by greek leter eta (η)*/
+#define TMP468_REG_REMOTE6_THERM1_LIMIT 0x6a
+#define TMP468_REG_REMOTE6_THERM2_LIMIT 0x6b
+
+
+#define TMP468_REG_REMOTE7_OFFSET 0x70
+#define TMP468_REG_REMOTE7_IDEALITY_CORRECTION 0x71/*on datasheet represented by greek leter eta (η)*/
+#define TMP468_REG_REMOTE7_THERM1_LIMIT 0x72
+#define TMP468_REG_REMOTE7_THERM2_LIMIT 0x73
+
+
+#define TMP468_REG_REMOTE8_OFFSET 0x78
+#define TMP468_REG_REMOTE8_IDEALITY_CORRECTION 0x79/*on datasheet represented by greek leter eta (η)*/
+#define TMP468_REG_REMOTE8_THERM1_LIMIT 0x7a
+#define TMP468_REG_REMOTE8_THERM2_LIMIT 0x7b
+
+#define TMP468_REG_BLOCKREAD_START 0x80 /* reading from this address trigger autoincrement of pointer register*/
+#define TMP468_REG_BLOCKREAD_END 0x88 /*inclusive*/
+
+#define TMP468_REG_REMOTE_CONFIG_BASE_ADDR TMP468_REG_REMOTE1_OFFSET /*!< address of REMOTE1 sensor*/
+#define TMP468_REG_REMOTE_CONFIG_REMOTE_REMOTE_OFFSET 0x8 /*!< address offset from REMOTE<n> to REMOTE<n+1> sensor*/
+#define TMP468_REG_REMOTE_CONFIG_REMOTE_OFFSET_OFFSET 0x0 /*!< address offset from the REMOTE sensor address to the offset config register*/
+#define TMP468_REG_REMOTE_CONFIG_REMOTE_ETA_OFFSET 0x1    /*!< address offset from the REMOTE sensor address to the eta config register*/
+#define TMP468_REG_REMOTE_CONFIG_REMOTE_THERM1_OFFSET 0x2 /*!< address offset from the REMOTE sensor address to the THERM1 config register*/
+#define TMP468_REG_REMOTE_CONFIG_REMOTE_THERM2_OFFSET 0x3 /*!< address offset from the REMOTE sensor address to the THERM! config register*/
+
+
+/******************************* remote masking *******************************/
+
+#define TMP468_REMOTE_bitmask (0xff<<8)
+#define TMP468_REMOTE8_MASK   0b1000000000000000
+#define TMP468_REMOTE7_MASK   0b0100000000000000
+#define TMP468_REMOTE6_MASK   0b0010000000000000
+#define TMP468_REMOTE5_MASK   0b0001000000000000
+#define TMP468_REMOTE4_MASK   0b0000100000000000
+#define TMP468_REMOTE3_MASK   0b0000010000000000
+#define TMP468_REMOTE2_MASK   0b0000001000000000
+#define TMP468_REMOTE1_MASK   0b0000000100000000
+#define TMP468_LOCAL_MASK     0b0000000010000000
+
+#define TMP468_CONFIG_ENABLE_REMOTE_BASE   TMP468_REMOTE1_MASK /* shift left to remote index*/
+#define TMP468_CONFIG_ENABLE_LOCAL         (1U<<7)
+#define TMP468_CONFIG_ONESHOT_START        (1U<<6)
+#define TMP468_CONFIG_SHUTDOWN_mask        (1U<<5)
+#define TMP468_CONFIG_CONVERSION_RATE_mask (7U<<2)
+#define TMP468_CONFIG_CONVERSION_RATE_pos  (2U)
+#define TMP468_CONFIG_BUSY                 (1U<<1)
+
+#define TMP468_CONVERSION_RATE_16s         (0U<<TMP468_CONFIG_CONVERSION_RATE_pos)
+#define TMP468_CONVERSION_RATE_8s          (1U<<TMP468_CONFIG_CONVERSION_RATE_pos)
+#define TMP468_CONVERSION_RATE_4s          (2U<<TMP468_CONFIG_CONVERSION_RATE_pos)
+#define TMP468_CONVERSION_RATE_2s          (3U<<TMP468_CONFIG_CONVERSION_RATE_pos)
+#define TMP468_CONVERSION_RATE_1s          (4U<<TMP468_CONFIG_CONVERSION_RATE_pos)
+#define TMP468_CONVERSION_RATE_2hz         (5U<<TMP468_CONFIG_CONVERSION_RATE_pos)
+#define TMP468_CONVERSION_RATE_3hz         (6U<<TMP468_CONFIG_CONVERSION_RATE_pos)
+#define TMP468_CONVERSION_RATE_CONTINUOUS  (7U<<TMP468_CONFIG_CONVERSION_RATE_pos)
+
+#define TMP468_THERM_mask           0b0111111111000000
+#define TMP468_THERM_DISABLE        TMP468_THERM_mask
+#define TMP468_THERM_pos            (6U)
+#define TMP468_THERM_SIGNAL_mask    1U<<15
+#define TMP468_SELECT_THERM1        0
+#define TMP468_SELECT_THERM2        1
+
+#define TMP468_HYSTERESIS_pos       7U
+#define TMP468_HYSTERESIS_mask      (0xff << TMP468_HYSTERESIS_pos)
+
+/******************************* Info Registers *******************************/
+
+#define TMP468_REG_LOCK  0xC4  /* Lock register */
+#define TMP468_REG_MANID 0xFE  /* Manufacturer identification 0x5449 */
+#define TMP468_REG_DEVID 0xFF  /* Device identification 0x0468 */
+
+/****************************** Resolution info *******************************/
+#define TMP468_HYSTERESIS_RESOLUTION 1        /*!< one degree per bit*/
+#define TMP468_THERM_RESOLUTION      0.5f     /*!< 0.5 degrees per bit*/
+#define TMP468_TEMP_RESOLUTION       0.0625f  /*!< 0.0625 degrees per bit*/
+
+/********************************* Constants **********************************/
+#define TMP468_LOCK         0xEB19
+#define TMP468_UNLOCK       0x5CA6
+#define TMP468_LOCKED       0x8000
+#define TMP468_UNLOCKED     0x0000
+
+#define TMP468_REMOTE_CONFIG_STRUCT
+
+#ifdef TMP468_REMOTE_CONFIG_STRUCT
+struct TMP468_remoteConfig{
+    int32_t offset;
+    int8_t idealityCorrection;
+    int32_t THERM1_limit; 
+    int32_t THERM2_limit;
+};
+#endif //TMP468_REMOTE_CONFIG_STRUCT
+
+typedef struct TMP468_t{
+    uint16_t addr;
+    // uint16_t config;
+    uint16_t hysteresis;
+    uint16_t localTHERMLimit1, localTHERMLimit2;
+    #ifdef TMP468_REMOTE_CONFIG_STRUCT
+    struct TMP468_remoteConfig remote[8];
+    #endif //TMP468_REMOTE_CONFIG_STRUCT
+    void (*i2c_write)(uint16_t, uint8_t, uint16_t); //addr, reg, pData
+    uint16_t (*i2c_read )(uint16_t, uint8_t); //addr, reg
+} TMP468_t;
+
+// Python reference, table12 of the datasheet (revB)
+// etatoi = lambda x: round( ((1.008*2088)/x)-2088 )
+// itoeta = lambda x: (1.008*2088)/(2088+x)
+#define TMP468_etatoi(eta) ((int)((((1.008f * 2088.0f) / (eta)) - 2088.0f) + 0.5f))
+#define TMP468_itoeta(i)   ((1.008f * 2088.0f) / (2088.0f + (float)(i)))
+
+int32_t     TMP468_getRemoteTemperature(TMP468_t target, int idx);
+uint16_t    TMP468_getTHERM1Status(TMP468_t target);
+uint16_t    TMP468_getTHERM2Status(TMP468_t target);
+uint16_t    TMP468_getOpenStatus(TMP468_t target);
+int         TMP468_isbusy(TMP468_t target);
+TMP468_t    TMP468_init(uint16_t addr, uint16_t conversionRate, uint16_t hysteresis, uint32_t THERM1, uint32_t THERM2);
+void        TMP468_setRemoteEnabled(TMP468_t target, uint16_t flags);
+uint16_t    TMP468_getRemoteEnabled(TMP468_t target);
+void        TMP468_enableLocal(TMP468_t target);
+void        TMP468_disableLocal(TMP468_t target);
+uint16_t    TMP468_getHysteresis(TMP468_t target);
+void        TMP468_setHysteresis(TMP468_t target, uint16_t hysteresis);
+int16_t     TMP468_getThermlimit(TMP468_t target, int idx, int thermSelect);
+void        TMP468_setThermLimit(TMP468_t target, int idx, int16_t thermlimit, int thermSelect);
+void        TMP468_disableTherm(TMP468_t target, int idx, int thermSelect);
+void        TMP468_configRemote(TMP468_t target, int idx, uint32_t offset, int8_t eta, uint32_t THERM1, uint32_t THERM2);
+void        TMP468_wakeup(TMP468_t target);
+void        TMP468_sleep(TMP468_t target);
+void        TMP468_lock(TMP468_t target);
+void        TMP468_unlock(TMP468_t target);
+uint16_t    TMP468_getLock(TMP468_t target);
+
+
+#endif /* INC_TMP468_HO_ */
--- Core/Src/TMP468.c
+++ Core/Src/TMP468.c
@@ -7,17 +7,296 @@
  */
 
 
-#include <TMP468.h>
 #include "main.h"
 #include "util.h"
+#include "TMP468.h"
 
-#define TMP468_addr 0x48
-#define blockRead_addr 0x80
-//Number of blocks in block read mode. internal tmp + 8 peripherals
-#define nBlocks 9
-uint16_t temps[nBlocks]; //Holds the values read from i2c
+/**
+  * Return temperature convreted to thousandths of degree.
+  * resolution of original signal is 0.0625 degC
+  */
+int32_t regToMillis(int16_t reg){
+  // Using division here since bitwise shift with signal extend is not 
+  // a guaranteed behavior of C.
+  return (int32_t)(reg / 8) * 62;
+}
 
-void readTemps(){
-	//write address to the pointer register
+/**
+ * @brief transform a temperature in milidegrees into the format expecified by the register.
+ * Since the actual resolution of the device is TMP468_TEMP_RESOLUTION per bit, for data storage
+ * it is prefered to either convert to degrees, or into the register format, which fits in a 16 bit
+ * signed.
+ * 
+ * @param millis 
+ * @return int16_t 
+ */
+int16_t millisToReg(int32_t millis){
+	int16_t reg = millis / 62; //truncate if necessary
+	return reg<<3;
+}
 
+
+/**
+ * @brief input value is degreeCelsius * 2 to keep resolution of register.
+ * output can be directly written to THERM Limit register
+ * 
+ * @param hlvcelsius temperature in half degree resolution in range 
+ * @return uint16_t bitfield on correct format for i2c write
+ */
+uint16_t halfDegreesToThermLimitreg(int hlvcelsius){
+	return hlvcelsius << TMP468_THERM_pos;
+}
+
+/**
+ * @brief returned value is degreeCelsius * 2 to keep resolution of register.
+ * 
+ * @param thermlimit raw register contents of THERM limit
+ * @return int Temperature in half-degrees
+ */
+int thermLimitRegtoHalfDegrees(uint16_t thermlimit){
+	int hlvcelsius = (thermlimit & TMP468_THERM_mask) >> TMP468_THERM_pos;
+	return (thermlimit&TMP468_THERM_SIGNAL_mask) ? -hlvcelsius : hlvcelsius;
+}
+
+/********************************** Reading ***********************************/
+
+/**
+ * @brief Returns temperature of channel idx in thousandths of degrees
+ * 
+ * @param target 
+ * @param idx Remote sensor channel. Zero if using local sensor
+ * @return uint32_t 
+ */
+int32_t TMP468_getRemoteTemperature(TMP468_t target, int idx){
+	if (idx<0 || idx>8) return -1;
+	return regToMillis(target.i2c_read(target.addr, TMP468_REG_LOCAL_TMP + idx));
+}
+
+/**
+ * @brief Returns status of the therm alerts
+ * 
+ * Returns a bitfield with a bit set when a channel has exceeded the THERM1 limit. 
+ * Bit is reset when the temperature goes bellow THERM-Hysteresis.
+ * Use the masks TMP468_REMOTE*_MASK and TMP468_LOCAL_MASK for checking each channel independently.
+ * 
+ * Warning: This is the inverse of the alert pin on the IC, which goes to a logical low when the sensor
+ * detects overtemperature.
+ * 
+ * @param target 
+ * @return uint16_t 
+ */
+uint16_t TMP468_getTHERM1Status(TMP468_t target){
+		return target.i2c_read(target.addr, TMP468_REG_THERM1_STATUS); 
+}
+
+
+/**
+ * @brief Returns status of the therm alerts
+ * 
+ * Returns a bitfield with a bit set when a channel has exceeded the THERM1 limit. 
+ * Bit is reset when the temperature goes bellow THERM-Hysteresis.
+ * Use the masks TMP468_REMOTE*_MASK and TMP468_LOCAL_MASK for checking each channel independently.
+ * 
+ * Warning: This is the inverse of the alert pin on the IC, which goes to a logical low when the sensor
+ * detects overtemperature.
+ * 
+ * @param target 
+ * @return uint16_t 
+ */
+uint16_t TMP468_getTHERM2Status(TMP468_t target){
+	return target.i2c_read(target.addr, TMP468_REG_THERM1_STATUS); 
+}
+
+/**
+ * @brief Check for connection state on the remote channels. 
+ * 
+ * Returns a bitfield with a bit set when an open circuit is detected. Use the masks
+ * TMP468_REMOTE*_MASK and TMP468_LOCAL_MASK for checking each channel independently.
+ * 
+ * @param target 
+ * @return uint16_t 0 if all circuits are closed, mask otherwise.
+ */
+uint16_t TMP468_getOpenStatus(TMP468_t target){
+	return target.i2c_read(target.addr, TMP468_REG_REMOTE_CHANNEL_OPEN_STATUS); 
+}
+
+/**
+ * @brief Busy status of the ADC in the TMP468
+ * 
+ * @param target 
+ * @return bool
+ */
+int TMP468_isbusy(TMP468_t target){
+	return target.i2c_read(target.addr, TMP468_REG_CONFIGURATION) & TMP468_CONFIG_BUSY;
+}
+
+/******************************* Configuration ********************************/
+
+TMP468_t TMP468_init(uint16_t addr, uint16_t conversionRate, uint16_t hysteresis, uint32_t THERM1, uint32_t THERM2){
+	TMP468_t target = {
+		.addr = addr,
+		.hysteresis = hysteresis,
+		.localTHERMLimit1 = THERM1,
+		.localTHERMLimit2 = THERM2
+	};
+
+	uint16_t cfg = target.i2c_read(addr, TMP468_REG_CONFIGURATION);	
+	cfg &= TMP468_CONFIG_CONVERSION_RATE_mask;
+	cfg |= conversionRate;
+	target.i2c_write(addr, TMP468_REG_CONFIGURATION, cfg);
+
+	target.i2c_write(target.addr, TMP468_REG_THERM_HYSTERESIS, hysteresis); 
+	target.i2c_write(target.addr, TMP468_REG_LOCAL_THERM2_LIMIT, millisToReg(THERM1));
+	target.i2c_write(target.addr, TMP468_REG_LOCAL_THERM1_LIMIT, millisToReg(THERM2));
+}
+
+void TMP468_setRemoteEnabled(TMP468_t target, uint16_t flags){
+	uint16_t cfg = target.i2c_read(target.addr, TMP468_REG_CONFIGURATION);
+	//clears bits
+	cfg &= ~TMP468_REMOTE_bitmask;
+	cfg |= flags & TMP468_REMOTE_bitmask;
+	target.i2c_write(target.addr, TMP468_REG_CONFIGURATION, cfg);
+}
+
+uint16_t TMP468_getRemoteEnabled(TMP468_t target){
+	uint16_t cfg = target.i2c_read(target.addr, TMP468_REG_CONFIGURATION);
+	cfg &= TMP468_REMOTE_bitmask;
+	return cfg;
+}
+
+void TMP468_enableLocal(TMP468_t target){
+	uint16_t cfg = target.i2c_read(target.addr, TMP468_REG_CONFIGURATION);
+	cfg |= TMP468_LOCAL_MASK; //sets bit to 1
+	target.i2c_write(target.addr, TMP468_REG_CONFIGURATION, cfg);
+}
+
+void TMP468_disableLocal(TMP468_t target){
+	uint16_t cfg = target.i2c_read(target.addr, TMP468_REG_CONFIGURATION);	
+	cfg &= ~TMP468_LOCAL_MASK; //sets bit to 1
+	target.i2c_write(target.addr, TMP468_REG_CONFIGURATION, cfg);
+}
+
+
+/**
+ * @brief Get value of hsteresis in degrees
+ * 
+ * @param target 
+ * @return int16_t 
+ */
+uint16_t TMP468_getHysteresis(TMP468_t target){
+	target.i2c_read(target.addr, TMP468_REG_THERM_HYSTERESIS) >> TMP468_HYSTERESIS_pos;
+}
+
+/**
+ * @brief Sets hysteresis
+ * 
+ * @param target 
+ * @param hysteresis Hysteresis temperature in degrees
+ */
+void TMP468_setHysteresis(TMP468_t target, uint16_t hysteresis){
+	target.i2c_write(target.addr, TMP468_REG_THERM_HYSTERESIS, hysteresis << TMP468_HYSTERESIS_pos);
+}
+
+//Note, idx0 is the local
+int16_t thermRegisterOffset(int idx, int thermSelect){
+	if (idx < 0 || idx > 8 || thermSelect < 0 || thermSelect > 1) return -1;
+
+	int16_t reg;
+	if (idx==0)
+	{
+		reg = TMP468_REG_LOCAL_THERM2_LIMIT + thermSelect;
+	} else {
+		reg = TMP468_REG_REMOTE_CONFIG_BASE_ADDR + \
+		      (TMP468_REG_REMOTE_CONFIG_REMOTE_REMOTE_OFFSET * idx) + \
+			  TMP468_REG_REMOTE_CONFIG_REMOTE_THERM1_OFFSET + \
+			  thermSelect;
+	}
+	return reg;
+}
+
+int16_t TMP468_getThermlimit(TMP468_t target, int idx, int thermSelect){
+	int16_t reg = thermRegisterOffset(idx, thermSelect);
+	if (reg == -1) return;
+	return target.i2c_read(target.addr, reg);
+}
+
+void TMP468_setThermLimit(TMP468_t target, int idx, int16_t thermlimit, int thermSelect){
+	int16_t reg = thermRegisterOffset(idx, thermSelect);
+	if (reg == -1) return;
+	target.i2c_write(target.addr, reg, thermlimit);
+}
+
+void TMP468_disableTherm(TMP468_t target, int idx, int thermSelect){
+	TMP468_setThermLimit(target, idx, thermSelect, TMP468_THERM_DISABLE);
+}
+
+/**
+ * @brief Configure the remote temperature sensor number IDX.
+ * 
+ * @param target Target Temperature sensor
+ * @param idx Target remote sensor index (from 1 to 8)
+ * @param offset Temperature offset to apply to measurement, in mili degrees [–128000, +127937]
+ * @param eta ideality factor correction. Convert from float using TMP468_etatoi for compile time optimization.
+ * @param THERM1 Temperature alarm 1, in mili degrees
+ * @param THERM2 Temperature alarm 2, in mili degrees
+ */
+void TMP468_configRemote(TMP468_t target, int idx, uint32_t offset, int8_t eta, uint32_t THERM1, uint32_t THERM2){
+	//base address
+	unsigned baseRegister = TMP468_REG_REMOTE_CONFIG_BASE_ADDR + (TMP468_REG_REMOTE_CONFIG_REMOTE_REMOTE_OFFSET * idx);
+	unsigned offsetRegister = baseRegister + TMP468_REG_REMOTE_CONFIG_REMOTE_OFFSET_OFFSET;
+	unsigned etaResister = baseRegister + TMP468_REG_REMOTE_CONFIG_REMOTE_ETA_OFFSET;
+	unsigned THERM1Register = baseRegister + TMP468_REG_REMOTE_CONFIG_REMOTE_THERM1_OFFSET;
+	unsigned THERM2Register = baseRegister + TMP468_REG_REMOTE_CONFIG_REMOTE_THERM2_OFFSET;
+
+	target.i2c_write(target.addr, offsetRegister, millisToReg(offset));
+	target.i2c_write(target.addr, etaResister, eta);
+	target.i2c_write(target.addr, THERM1Register, halfDegreesToThermLimitreg(THERM1/2000));
+	target.i2c_write(target.addr, THERM2Register, halfDegreesToThermLimitreg(THERM2/2000));
+
+	//set the configured remote as enabled
+	uint16_t enabled = TMP468_getRemoteEnabled(target);
+	enabled |= TMP468_CONFIG_ENABLE_REMOTE_BASE << idx;
+	TMP468_setRemoteEnabled(target, enabled);
+	
+	#ifdef TMP468_REMOTE_CONFIG_STRUCT
+	target.remote[idx-1].offset = offset;
+	target.remote[idx-1].idealityCorrection = eta;
+	target.remote[idx-1].THERM1_limit = THERM1;
+	target.remote[idx-1].THERM2_limit = THERM2;
+    #endif //TMP468_REMOTE_CONFIG_STRUCT
+}
+
+/**
+ * @brief Enable temperature gathering according to configuration
+ * 
+ * @param target 
+ */
+void TMP468_wakeup(TMP468_t target){
+	uint16_t cfg = target.i2c_read(target.addr, TMP468_REG_CONFIGURATION);
+	cfg |= TMP468_CONFIG_SHUTDOWN_mask; //sets bit to 1
+	target.i2c_write(target.addr, TMP468_REG_CONFIGURATION, cfg);
+}
+
+/**
+ * @brief enters low power mode
+ * 
+ * @param target 
+ */
+void TMP468_sleep(TMP468_t target){
+	uint16_t cfg = target.i2c_read(target.addr, TMP468_REG_CONFIGURATION);
+	cfg &= ~TMP468_CONFIG_SHUTDOWN_mask; //clear bit
+	target.i2c_write(target.addr, TMP468_REG_CONFIGURATION, cfg);
+}
+
+/******************************** Lock control ********************************/
+void TMP468_lock(TMP468_t target){
+	target.i2c_write(target.addr, TMP468_REG_LOCK, TMP468_LOCK);
+}
+
+void TMP468_unlock(TMP468_t target){
+	target.i2c_write(target.addr, TMP468_REG_LOCK, TMP468_UNLOCK);
+}
+
+uint16_t TMP468_getLock(TMP468_t target){
+	return target.i2c_read(target.addr, TMP468_LOCK);
 }
